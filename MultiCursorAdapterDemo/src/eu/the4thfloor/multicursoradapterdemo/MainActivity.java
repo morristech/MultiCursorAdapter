0099b2ef7eff0d4c6508d64d5ff11df282f37492
Ralph Bergmann <ralph@the4thfloor.eu>
2013-04-14 22:02:26 +0200

    init

---
+++ MultiCursorAdapterDemo/src/eu/the4thfloor/multicursoradapterdemo/MainActivity.java
@@ -0,0 +1,16 @@
+
+package eu.the4thfloor.multicursoradapterdemo;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends ListActivity {
+
+
+  @Override
+  protected void onCreate(final Bundle savedInstanceState) {
+
+    super.onCreate(savedInstanceState);
+  }
+}
